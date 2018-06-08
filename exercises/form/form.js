64a67da019cf50007f7f5a6387b978cd90dc6984
Taylor Burke <twillyb@gmail.com>
2018-06-08 11:49:04 -0600

    push open west conf

--- exercises/form/form.js
+++ exercises/form/form.js
@@ -1,11 +1,14 @@
 var form = document.demoForm;
 
 form.addEventListener("submit", function (e) {
+    debugger;
     e.preventDefault();
     var output = {};
     output.name = this.name.value;
+    output.name = this.name2.value;
     output.email = this.email.value;
-    output.password = this.password.value;
+    output.age = this.age.value;
+    output.gender = this.gender.value;
     console.log(output);
 })
     
