23e89a92b6934f8811d6f341427205a68c509bbe
Taylor Burke <twillyb@gmail.com>
2018-09-06 12:06:07 -0600

    prepare new site

---
+++ Sara/min-portfolio/src/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Header.css';
+import Headline from '../Menu/Headline';
+
+
+const Header = (props) => {
+    console.log(props);
+    return (
+        <div className="App-header">
+            <Headline scrollToContact={props.scrollToContact} scrollToProject={props.scrollToProject}/>
+
+            <div className="App-title">
+
+
+                <h1>Hi! I'm Taylor Burke.
+            I make sublime websites.</h1>
+
+            </div>
+
+            <div className="Fade"> </div>
+        </div>
+    )
+}
+
+export default Header;
