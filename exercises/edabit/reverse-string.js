9523e7cc78e1edb925be0ec1668df598182bf561
Taylor Burke <twillyb@gmail.com>
2018-09-07 21:07:02 -0600

    new edabit completed

--- exercises/edabit/reverse-string.js
+++ exercises/edabit/reverse-string.js
@@ -1,5 +1,13 @@
-string = "fudge"
+string = "test"
 
-function reverse(str) {
-	
-}
+function reverseString(str) {
+        let splitString = str.split(""); // get array of each index
+        let reverseArray = splitString.reverse(); // reverse array
+     
+
+        let joinArray = reverseArray.join(""); // create string from reversed array
+        
+        return joinArray; 
+    }
+     
+console.log(reverseString(string));
