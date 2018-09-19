6af72809ad63d7a2383c3c433d469aeecbdf3b89
Taylor Burke <twillyb@gmail.com>
2018-09-19 11:15:46 -0600

    self-edabit

---
+++ exercises/edabit/self-challenge.js
@@ -0,0 +1,20 @@
+//  challenge: write a function that takes two arguments
+// first is the base number
+// second is the exponent of that number
+// ie (5, 5) would return 5 times itself 5 times, or 3125
+// note; this includes the first instance, so it looks like:
+// (5 * 5 * 5 * 5 * 5)
+
+exponent = 5;
+number = 5;
+
+const scientificNotation = ( num, exp ) => {
+    let answer = 0;
+
+
+    return answer
+}
+
+
+
+console.log(scientificNotation( number, exponent ));
