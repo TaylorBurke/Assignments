aabeaccd8db6ed99c28d08dad05884b9794c2288
Taylor Burke <twillyb@gmail.com>
2018-10-02 08:34:06 -0600

    new edabit

---
+++ exercises/edabit/count-digits.js
@@ -0,0 +1,12 @@
+// https://edabit.com/challenge/MxXj9ZhTGfA96uPm7
+
+
+number = 453;
+
+function findDigitAmount(num) {
+    let n = num.toString();
+    let answer = n.length;
+    return answer
+}
+
+console.log(findDigitAmount(number));
