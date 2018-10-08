ba4156b1eb02c56a12ecc2f9f7a786d1ad3ea0fe
Taylor Burke <twillyb@gmail.com>
2018-10-07 20:16:54 -0600

    edabit

---
+++ exercises/edabit/absolute-value.js
@@ -0,0 +1,21 @@
+// https://edabit.com/challenge/rCmEy2AQYLbRGgKyL
+
+let array = [1, -2, 3, 4];
+
+
+
+
+function getAbsSum(arr) {
+	return Math.abs(arr)
+}
+
+
+const array1 = [1, -2, 3, 4];
+
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+
+console.log(array1.reduce(reducer));
+
+console.log(getAbsSum(array));
---
+++ exercises/edabit/reduce.js
@@ -0,0 +1,7 @@
+const array1 = [1, -2, 3, 4];
+
+
+const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+
+console.log(array1.reduce(reducer));
