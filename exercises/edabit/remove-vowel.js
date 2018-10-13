51e8aa7630b88a1a047043e236ee5447ced20b57
Taylor Burke <twillyb@gmail.com>
2018-10-13 12:23:15 -0600

    edabit

--- exercises/edabit/remove-vowel.js
+++ exercises/edabit/remove-vowel.js
@@ -3,13 +3,31 @@
 let str = "lazer engraved testicles"
 
 function silenceTrump(str) {
-	for (i = 0; i < str.length; i++){
-        if(str[i]=== "a" | "A"){
-            str[i] = ""
+	for (let i = 0; i < str.length; i++){
+        
+        if (str[i] === "a"){
+            str[i] = "";
+        }
+
+        if (str[i] === "e"){
+            str[i] = "";
+        }
+
+        if (str[i] === "i"){
+            str[i] = "";
+        }
+        if (str[i] === "o"){
+            str[i] = "";
+        }
+        if (str[i] === "u"){
+            str[i] = "";
+        }
+        else {
+            str[i] = str[i]
         }
         return str
     }
-
+ 
 }
 
 console.log(silenceTrump(str))
