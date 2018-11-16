26990c1b53cce477a45dc5cc781a6acd144cbe19
Taylor Burke <twillyb@gmail.com>
2018-11-16 08:34:32 -0700

    edabit

--- exercises/edabit/num-sort.js
+++ exercises/edabit/num-sort.js
@@ -1,5 +1,13 @@
 // https://edabit.com/challenge/yaXQvCzAXJLe37Qie
 
+let num = 123232145;
 function sortDecending(num) {
-	
+    let arr = [];
+    for (i = 0; i < num.length; i++){
+        arr.push(num[i])
+    }
+    arr.push(num);
+    return arr
 }
+
+console.log(sortDecending(num))
