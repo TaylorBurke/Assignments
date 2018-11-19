1a42bab93684b3a7332ef49e76c7cc8f5c5de94d
Taylor Burke <twillyb@gmail.com>
2018-11-18 20:51:22 -0700

    testers

---
+++ exercises/p-i/obj.js
@@ -0,0 +1,49 @@
+const Tester = {
+
+    Name: "tester",
+    
+    Age: 54,
+    
+    Id: 125468
+    
+    };
+
+// console.log(Object.keys(Tester));
+
+TesterArr = [
+
+    {
+    
+    Name: 'tester one',
+    
+    Age: 20,
+    
+    Id: 6548
+    
+    },
+    
+    {
+    
+    Name: 'tester two',
+    
+    Age: 20,
+    
+    Id: 456488
+    
+    },
+    
+    {
+    
+    Name: 'tester three',
+    
+    Age: 20,
+    
+    Id: 73453
+    
+    },
+    
+    ]
+
+for (i = 0; i < TesterArr.length; i ++) {
+console.log(`array object names: ${TesterArr[i].Name}`)
+}
