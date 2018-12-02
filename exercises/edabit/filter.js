f1d8783a7505d88e69b0da717edb24a722f0aea4
Taylor Burke <twillyb@gmail.com>
2018-12-01 17:37:11 -0700

    edabit

--- exercises/edabit/filter.js
+++ exercises/edabit/filter.js
@@ -1,6 +1,5 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
 
-
 const oils = [
     'peanut',
     'coconut',
@@ -16,7 +15,9 @@ oils.push('vegetable', 'MCT', 'definitelynotpalmoil');
 
 // this function filters through the array of oils and removes any containing the phrase 'palm'
 
-const rainforestSafe = oils.filter((oil) => !(oil.match(/palm/)) )
+const rainforestSafe = oils.filter((oil) => !(oil.match(/palm/)));
 
+const hardToRead = oils.filter((oil) => oil.length > 6);
 
 console.log(rainforestSafe);
+console.log(hardToRead);
