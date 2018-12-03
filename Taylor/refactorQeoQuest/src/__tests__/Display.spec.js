d03dfbe1925f7014a6ba1a854200c6d60335d381
Taylor Burke <twillyb@gmail.com>
2018-12-02 17:28:03 -0700

    test test suite

--- Taylor/refactorQeoQuest/src/__tests__/Display.spec.js
+++ Taylor/refactorQeoQuest/src/__tests__/Display.spec.js
@@ -1,6 +1,13 @@
-describe(
-    // it ( " should align center ")
+const objectThing = {
+    test: 1,
+    show: 0,
+};
 
-    // it ( " should produce flag ")
-
-    
+describe('the objectThing', ()=>{
+    it('should have a test property that is truthy', ()=>{
+        expect(objectThing.test).toBeTruthy()
+    });
+    it('should have a show property that is falsy', ()=>{
+        expect(objectThing.show).toBeFalsy()
+    });
+});
---
+++ Taylor/refactorQeoQuest/src/thing.js
@@ -0,0 +1,6 @@
+const objectThing = {
+    test: 1,
+    property: 0,
+};
+
+export default objectThing;
