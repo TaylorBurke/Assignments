a3af14363bfde18409c6222439b9df3fced8ee77
Taylor Burke <twillyb@gmail.com>
2019-01-07 20:58:02 -0700

    finish rock paper sci

--- Taylor/rock-paper-sci.js
+++ Taylor/rock-paper-sci.js
@@ -2,16 +2,31 @@
 
 const rockPaperScissors = (move) => {
     const computerMove = 
-    Math.random() * Math.floor(3);
-    console.log(computerMove);
+    Math.floor(Math.random() * Math.floor(3)) +1;
 
-    const playerMove = (move) => {
-    if ( move === "rock" ) return 1;
-    else if ( move === "scissors") return 2;
-    else if (move === "paper") return 3;
+    if ( move === "rock" ) playerMove = 1;
+    else if ( move === "scissors") playerMove = 2;
+    else if (move === "paper") playerMove = 3;
+
+    if (playerMove === 1 && computerMove === 3){
+        return("The computer covered your rock with paper!")
     }
-    console.log(playerMove(move));
-
+    else if (playerMove === 2 && computerMove === 1){
+        return("The computer smashed your scissors with rock!")
+    }
+    else if (playerMove === 3 && computerMove === 2){
+        return("The computer cut your paper with scissors!")
+    }
+    else if (playerMove === 1 && computerMove === 2){
+        return("You smash the computer's scissors with your rock!")
+    }
+    else if (playerMove === 2 && computerMove === 3){
+        return("You cut the computer's paper with your scissors!")
+    }
+    else if (playerMove === 3 && computerMove === 1){
+        return("You cover the computer's rock with your paper!")
+    }
+    else return("You both threw the same move, it's a draw!")
     };
 
-console.log(rockPaperScissors("rock"));
+console.log(rockPaperScissors("scissors"));
