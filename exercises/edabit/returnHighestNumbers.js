db436f0424ba37d5a53dc72f2c8bf96ca2f15fb3
Taylor Burke <twillyb@gmail.com>
2019-02-18 12:36:02 -0700

    edabit

--- exercises/edabit/returnHighestNumbers.js
+++ exercises/edabit/returnHighestNumbers.js
@@ -3,19 +3,11 @@ const arr = [ [-9, 1,3], [1,5,6] ]
 function findLargestNums(arr) {
     let answer = [];
 	
-		answer.push(arr.forEach(array => {
-            array.reduce((acc, cv) => {
-                
-                if (cv > acc) {
-                    acc = cv;
-                }
-                return acc;
-            })
-        })
-    )
+	arr.forEach(array => {
+        answer.push(Math.max(...array));
+    })
     return answer;
 }
 
-// should return [3, 8]
 
 console.log(findLargestNums(arr));
