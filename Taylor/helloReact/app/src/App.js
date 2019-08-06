4b276717d2acafe024e15e3d4d2469a6e6396a19
Taylor Burke <twillyb@gmail.com>
2019-08-05 20:57:08 -0600

    clean start

--- Taylor/helloReact/app/src/App.js
+++ Taylor/helloReact/app/src/App.js
@@ -1,24 +1,16 @@
-cd import React from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import logo from './te.png';
 import './App.css';
+import Hidden from './hidden';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Hidden className="text"/>
       </header>
+
     </div>
   );
 }
---
+++ Taylor/helloReact/app/src/hidden.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hidden = () => {
+    return (
+        <div>
+            <p>
+                The nameless is the origin of Heaven and Earth
+            </p>
+        </div>
+    )
+}
+
+export default Hidden;
