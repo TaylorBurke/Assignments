541e769ada580be63185b0dc0a7e327f047f3a09
Taylor Burke <twillyb@gmail.com>
2020-02-02 20:28:32 -0700

    solved

--- exercises/edabit/compoundInterest.js
+++ exercises/edabit/compoundInterest.js
@@ -3,27 +3,9 @@
 // Create a function that accepts the principal p, the term in years t, the interest rate r, and the number of compounding periods per year n. The function returns the value at the end of term rounded to the nearest cent.
 
 const compoundInterest = (p, t, r, n) => {
-    let principal = p;
-    let termInYears = t;
-    let interestRate = r;
-    let numberOfCompoundsPerYear = n;
-
-
-    const getReturn = () => {
-        let totalNumberOfCompounds = numberOfCompoundsPerYear * termInYears;
-        let accumulator = principal;
-        let modifier = interestRate +1;
-
-        for (i = 0; i++; i < totalNumberOfCompounds) {
-            let newValue = accumulator * modifier;
-            accumulator = newValue;
-        }
-
-        return accumulator;
-    }
-
-    return getReturn();
-
+let modifier = t * n;
+let interest = p * (Math.pow((1 + r / n), modifier)) - p;
+return +(interest + p).toFixed(2);
 }
 
 console.log(compoundInterest(10000, 10, 0.06, 12));
