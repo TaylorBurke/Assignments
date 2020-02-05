bded01a5df77f4f70374f9194e87e7d7a9120a7a
Taylor Burke <twillyb@gmail.com>
2020-02-04 20:24:54 -0700

    wip

--- Taylor/rsvp/my-app/src/App.js
+++ Taylor/rsvp/my-app/src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from '@material-ui/core';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <a target="blank" style={{ textDecoration: "none" }} href="https://docs.google.com/forms/d/e/1FAIpQLSdm3nXK3HXZNAo2LQVgKeXpqj3Shj9Ydd4P87uS3DHkUixzIA/viewform?usp=sf_link">
+        <Button variant="contained" color="primary" className="button">RSVP for Missouri</Button>
+      </a>
+
+      <br />
+
+      <a target="blank" style={{ textDecoration: "none" }} href="https://docs.google.com/forms/d/e/1FAIpQLSdm3nXK3HXZNAo2LQVgKeXpqj3Shj9Ydd4P87uS3DHkUixzIA/viewform?usp=sf_link">
+        <Button variant="contained" color="primary" className="button">RSVP for Utah</Button></a>
+
     </div>
   );
 }
